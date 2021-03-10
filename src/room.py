dff17cf2d41a47fae0fb705b568f85a9cfb24a4b
unknown <tompsherman@gmail.com>
2021-03-10 17:39:27 -0500

    room class initiated

--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"You are in the {self.name}:\n\n\t{self.description}"
+
+office = Room("office", "your man-made prison with a beautiful outlook of nature")
+
+# print(office)
