a8af708c64a528def6c7c66312d322857b0e27e4
unknown <tompsherman@gmail.com>
2021-03-10 17:39:11 -0500

    player class initiated

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    
+    def __init__(self, location):
+        self.location = location
+
+    def __str__(self):
+        return f"{self.location}"
+
+tom = Player("office")
+
+# print(tom)
