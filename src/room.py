827a26b406681c13885765bec2f2d50b9de45928
unknown <tompsherman@gmail.com>
2021-03-13 19:52:11 -0500

    adding items to rooms

--- src/room.py
+++ src/room.py
@@ -2,13 +2,22 @@
 # description attributes.
 class Room:
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
+        self.items = items
 
     def __str__(self):
-        return f"You are in the {self.name}:\n\n\t{self.description}\n"
+        output = f"You are in the {self.name}:\n\n\t{self.description}\n"
 
-office = Room("office", "your man-made prison with a beautiful outlook of nature")
+        if len(self.items) > 0:
+            for i, item in enumerate(self.items):
+                output += f"\t[{i}]  {item}\n"
+        else:
+            output += f"there are no items to be found"
+
+        return output
+
+office = Room("office", "your man-made prison with a beautiful outlook of nature", [])
 
 # print(office)
